5bd66da169fd3dd71931ceb4a9e1377f08985a4c
Guilherme Pimentel <89157403+GuilhermeAPB@users.noreply.github.com>
2026-01-03 01:20:33 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // Se você usar Tailwind, este arquivo pode estar vazio ou ter as diretivas
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>,
+)
